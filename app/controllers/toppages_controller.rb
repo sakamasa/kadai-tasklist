d68744b53aa50a7fd2f91ea4dd6bba13e8cbacca
sakamasa <saka_masa1229@yahoo.co.jp>
2017-05-14 22:27:47 +0000

    login 2

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,4 +1,9 @@
 class ToppagesController < ApplicationController
   def index
+    if logged_in?
+      @user = current_user
+      @task = current_user.tasks.new
+      @tasks = current_user.tasks.order('updated_at DESC').page(params[:page])
+    end
   end
 end
